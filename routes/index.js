939bb87d849b624636d06480ae6c81868e30a80b
d <trd.johnlaurencedelara@gmail.com>
2020-07-13 14:00:54 +0800

    -initialize sequelize in www/bin

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var index = require('../Controllers/index_controller');
+var user = require('../Controllers/user_controller');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Starteer' });
-});
+router.get('/', index.index);
+
+router.get('/signup', user.show_signup);
+//router.post('/signup', user.signup);
 
 module.exports = router;
